5f10357d724963993a05e5e95abb5a47dfa1df67
Rune Tynan <runetynan@gmail.com>
2023-06-06 10:47:46 -0400

    Fmt

--- src/nan.rs
+++ src/nan.rs
@@ -20,17 +20,17 @@
 //! effectively a niche-optimized enum, where the niche is the NaN values of the stored float. This
 //! can save up to 8 bytes of memory for every object, for minimal runtime cast, which adds up
 //! quite fast.
-//! 
+//!
 //! ## Limitations
-//! 
+//!
 //! - Since all data must fit in the 6 bytes of space a float provides us, we cannot store
 //!   wide-pointers, so all data stored must be `Sized`. This can be worked around by using
 //!   double-indirection, storing a `Box<T: ?Sized>` or similar.
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! A general example of using [`NanBox`] with some large data type:
-//! 
+//!
 //! ```
 //! # use boxing::nan::NanBox;
 //!
@@ -40,7 +40,7 @@
 //!     class: u64,
 //!     fields: Vec<(String, NanBox<'a, InterpValue<'a>>)>,
 //! }
-//! 
+//!
 //! // We can make a list of boxes, which takes up the same amount of memory as a `Vec<f64>`
 //! let mut values = vec![
 //!     NanBox::from_float(1.0),
@@ -49,7 +49,7 @@
 //!         InterpValue { class: 1, fields: vec![(String::from("foo"), NanBox::from_float(-1.0))] }
 //!     )),
 //! ];
-//! 
+//!
 //! // We want to evaluate, say, +1 if the value is a float:
 //! for v in &mut values {
 //!     if let Some(f) = v.try_mut_float() {
@@ -59,14 +59,14 @@
 //!         *f += 1.0;
 //!     }
 //! }
-//! 
+//!
 //! // Alternatively, we want to read an object and get its fields:
 //! for v in &values {
 //!     if let Some(obj) = v.try_ref_boxed() {
 //!         println!("{}", obj.fields[0].0);
 //!     }
 //! }
-//! 
+//!
 //! // Final note: since every type is represented as a float, it's always safe and sound to turn
 //! // one into a float with no branches.
 //! for v in values {
@@ -74,7 +74,7 @@
 //!     println!("{}", v.into_float_unchecked());
 //! }
 //! ```
-//! 
+//!
 
 pub mod heap;
 pub mod raw;
